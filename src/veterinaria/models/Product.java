71b4795096debaf5e4841e520c4043be5339ae3b
JULIAN GIMENEZ <gimenezjulian1997@gmail.com>
2021-06-12 20:03:48 -0300

    Agruegue las clases catalogo y producto

---
+++ src/veterinaria/models/Product.java
@@ -0,0 +1,15 @@
+package veterinaria.models;
+
+public class Product {
+    private String productName;
+    private int code;
+    private float price;
+    private String description;
+
+    Product() {
+        productName = "";
+        code = 0;
+        price =0;
+        description = "";
+    }
+}
