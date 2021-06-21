893c58144410af61279d1ca7f6dfe622af4c2130
JULIAN GIMENEZ <gimenezjulian1997@gmail.com>
2021-06-20 21:14:19 -0300

    Trabaje en Schedule, faltan terminar 2 metodos por falta del getter de cliente

--- src/com/jmissart/models/store/Product.java
+++ src/com/jmissart/models/store/Product.java
@@ -1,8 +1,5 @@
 package com.jmissart.models.store;
 
 public class Product {
-<<<<<<< HEAD
 
-=======
->>>>>>> d3022256aa2d462007e66e745667de0cd3e699ac
 }
--- src/com/jmissart/utils/ICollections.java
+++ src/com/jmissart/utils/ICollections.java
@@ -2,7 +2,7 @@ package com.jmissart.utils;
 
 public interface ICollections {
     public void add(Object e);
-    public void create();
+    public void create(Object e);
     public String showCollection();
     public void update(Object e);
     public Object search(String data);
